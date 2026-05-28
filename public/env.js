042b19def07a56517d9bb6605ef1071d3523a8ea
cosaca taco <cosaca@ena-gifu.com>
2026-05-28 22:17:38 +0900

    Change Firebase Database URL in env.js
    Updated Firebase Database URL to point to the console.

--- public/env.js
+++ public/env.js
@@ -1,3 +1,3 @@
 // 🔒 あなたのFirebaseプロジェクトの「Realtime DatabaseのURL」に書き換えてください
-window.FIREBASE_DB_URL = "https://workbook-ai-taco-default-rtdb.asia-southeast1.firebasedatabase.app/";
+window.FIREBASE_DB_URL = "https://console.firebase.google.com/u/0/project/workbook-ai-taco/database/workbook-ai-taco-default-rtdb/data/~2F";
 
